ec4b963fb60430cff7e79a68f2da9967a540b531
Noahtv <Noahv.tv@usca.edu>
2020-12-09 04:56:10 -0500

    console

--- js/rpsgame.js
+++ js/rpsgame.js
@@ -5,14 +5,17 @@ var L = 0;
 function rockClick() {
     if(rN == 1) {
         document.getElementById('result').value="It's a tie!";
+        console.log("It's a tie");
     }
     else if(rN == 2) {
         document.getElementById('result').value="You lose!";
         L = L + 1;
+        console.log("You lose!");
     }
     else {
         document.getElementById('result').value="You Win!";
         W=W+1;
+        console.log("You Win!");
     }
 }
 
