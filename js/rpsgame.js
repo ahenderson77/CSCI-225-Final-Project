4cd28912b0be387b3e298a9be2fa815ca297536c
Noahtv <Noahv.tv@usca.edu>
2020-12-09 05:07:43 -0500

    random change

--- js/rpsgame.js
+++ js/rpsgame.js
@@ -1,13 +1,13 @@
-var rN = Math.floor((Math.random()*3)+1);
+var rN = Math.random()
 var W = 0;
 var L = 0;
 
 function rockClick() {
-    if(rN == 1) {
+    if(rN <=.33) {
         document.getElementById('result').value="It's a tie!";
         console.log("It's a tie");
     }
-    else if(rN == 2) {
+    else if(rN <=.66) {
         document.getElementById('result').value="You lose!";
         L = L + 1;
         console.log("You lose!");
